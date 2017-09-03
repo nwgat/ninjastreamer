132b02e5885cd478a2ccae5cd5a2592346113c4f
nwgat <github@nwgat.net>
2017-09-03 22:07:42 +0200

    Add files via upload

---
+++ ninjastreamer.720p.sh
@@ -0,0 +1,3 @@
+./nginx
+ffmpeg -r "60" -i rtmp://127.0.0.1:1935/live -map 0 -c:v libx264 -preset medium -level 4.1 -vf scale=1280:-1 -maxrate 3M -minrate 3M -bufsize 3M -g 120 -c:a aac -b:a 192k -strict -2 -flags +global_header -f flv rtmp://127.0.0.1/liveout
+
