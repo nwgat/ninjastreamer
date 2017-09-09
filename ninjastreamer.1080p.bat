a7898a19a7ac43f93eea41c8b208ed1220b80c9a
nwgat <github@nwgat.net>
2017-09-09 16:17:33 +0200

    Update ninjastreamer.1080p.bat

--- ninjastreamer.1080p.bat
+++ ninjastreamer.1080p.bat
@@ -1,2 +1,2 @@
 start nginx.exe
-start ffmpeg.exe -r "60" -i rtmp://127.0.0.1:1935/live -map 0 -c:v libx264 -preset medium -level 4.1 -maxrate 6M -minrate 6M -bufsize 6M -g 120 -c:a aac -b:a 192k -flags +global_header -f flv rtmp://127.0.0.1/liveout -threads 16
+start ffmpeg.exe -r "60" -i rtmp://127.0.0.1:1935/live -map 0 -c:v libx264 -preset medium -level 4.1 -maxrate 6M -minrate 6M -bufsize 6M -g 120 -c:a copy -flags +global_header -f flv rtmp://127.0.0.1/liveout -threads 16
